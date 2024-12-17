a31b99bb82d17fd8348f4ed0cf7917a6e389dad2
Suraj MS <47383168+suraj-ms@users.noreply.github.com>
2024-12-17 12:43:26 +0530

    Update App.jsx

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -68,7 +68,7 @@ function App() {
 
   return (
     <div className="game">
-      <h2>Tic Tac Tow</h2>
+      <h2 className="status">Tic Tac Tow</h2>
       <div className="game-board">
         <div className="board-row">
           {renderSquare(0)}
